d6659d750e4f0b732f0ebc94e1dcff2fda397dd5
tony tolver <andrademaresantonio00@gmail.com>
2023-10-02 23:11:28 -0600

    Tipo de datos

---
+++ js/03.js
@@ -0,0 +1,25 @@
+// Tipo de datos 
+
+//Undefined
+let cliente 
+console.log(typeof cliente);
+
+//String
+cliente = "Juan";
+console.log(typeof cliente);
+
+//Number
+cliente = 123;
+console.log(typeof cliente);
+
+//Boolean
+cliente = true;
+console.log(typeof cliente);
+
+//Null
+cliente = null;
+console.log(typeof cliente);
+
+//Symbol
+cliente = Symbol('Symbol');
+console.log(typeof cliente);
