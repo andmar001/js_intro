fbf3e830e88ee2613cae9d858143cac3965df723
tony tolver <andrademaresantonio00@gmail.com>
2023-10-03 00:36:31 -0600

    Intro to arreglos

---
+++ js/09.js
@@ -0,0 +1,7 @@
+// Arreglos 
+const tecnologias = ['HTML', 'CSS', 'JavaScript', 'PHP', 'VueJS', 'Laravel',29, true];
+console.table(tecnologias);
+
+// accder a indice del arreglo
+console.log(tecnologias[2]);
+
