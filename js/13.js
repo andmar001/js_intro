3f41216c7488c8667eba21fdd52610c659903402
tony tolver <andrademaresantonio00@gmail.com>
2023-10-03 10:25:22 -0600

    funciones intro

---
+++ js/13.js
@@ -0,0 +1,22 @@
+// function declaration
+
+function sumar(){
+  console.log(13+3)
+}
+
+sumar()
+
+// argumentos en los parametros
+function sumados(a,b){
+  console.log(a+b)
+}
+sumados(3,2)
+
+// funciones con parametros por default
+function sumatres(a=233,b=3){
+  console.log(a+b)
+}
+
+sumatres(2,2)
+// tomar los parametros por default 
+sumatres()
